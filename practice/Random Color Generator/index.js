ea1b76b627058fc75f33c73acaf73739271ab6bd
Henos Tefera <henostefera300@gmail.com>
2024-08-02 15:49:54 +0300

    random color generator

---
+++ practice/Random Color Generator/index.js
@@ -0,0 +1,29 @@
+const containerE1 = document.querySelector(".container");
+for (let index = 0; index < 30; index++) {
+    const colorContainerE1 = document.createElement("div");
+    colorContainerE1.classList.add("color-container");
+    containerE1.appendChild(colorContainerE1);
+}
+
+const colorContainerE1s = document.querySelectorAll(".color-container");
+
+
+generateColors();
+
+function generateColors(){
+    colorContainerE1s.forEach((colorContainerE1)=>{
+       const newColorCode = randomColor();
+       colorContainerE1.style.backgroundColor = "#"+ newColorCode;
+    });
+}
+
+function randomColor(){
+    const chars = "0123456789abcdef";
+    const colorCodeLenght = 6;
+    let colorCode = ""
+    for (let index = 0; index < colorCodeLenght; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+       colorCode += chars.substring(randomNum, randomNum + 1);    
+    }
+    return colorCode;
+}
