01077a9d863b9ec16f320543c8b13aa1065672f9
Henos Tefera <henostefera300@gmail.com>
2024-08-06 11:42:03 +0300

    Loan calculator

---
+++ practice/Animation Search Bar/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchBarContainerEl = document.querySelector(".search-bar-container");
+    const magnifierEl = document.querySelector(".magnifier");
+
+    magnifierEl.addEventListener("click", () => {
+        searchBarContainerEl.classList.toggle("active");
+    });
+});
